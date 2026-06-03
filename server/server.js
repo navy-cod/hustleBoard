8fde4f124a4f91322d3de178609cee23cac77bea
navY <pyvan223@gmail.com>
2026-06-03 05:56:41 -0400

    added default settings for render to connect to supabase db

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+const dns = require('dns');
+if (dns.setDefaultResultOrder) {
+    dns.setDefaultResultOrder('ipv4first');
+}
+
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
